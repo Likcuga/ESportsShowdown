c96a2451aa054d8ab23547193e050d2b7c35c1c1
Petar Zdravkovic <petarzdravkovic@gmail.com>
2023-03-31 14:40:59 +0200

    Some modifications

--- src/dev/pz/Main.java
+++ src/dev/pz/Main.java
@@ -6,14 +6,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Team<CSGO> fnatic = new Team<CSGO>("FNatic");
-        List <Player> fnaticRoster = new ArrayList();
-        fnatic.addTeamMember(new Player("Nico", "Mid"), fnaticRoster);
-        fnatic.addTeamMember(new Player("KRIMZ", "Short"), fnaticRoster);
-        fnatic.addTeamMember(new Player("mezii", "Short"), fnaticRoster);
-        fnatic.addTeamMember(new Player("roej", "Long"), fnaticRoster);
-        fnatic.addTeamMember(new Player("FASHR", "Long"), fnaticRoster);
-        fnatic.printTeamMembers(fnaticRoster);
+
+        Team<CSGO> fnatic = new Team<>("FNatic");
+        fnatic.initializeGameTeam(new Player("Nico", GameRole.MIDDLE),
+                new Player("KRIMZ", GameRole.SHORT),
+                new Player("mezii", GameRole.SHORT),
+                new Player("roej", GameRole.LONG),
+                new Player("FASHR", GameRole.LONG));
+        fnatic.printTeamMembers();
+
     }
 
+
+
 }
+
+//    Team<CSGO> fnatic = new Team<CSGO>("FNatic");
+//    List <Player> fnaticRoster = new ArrayList<>();
+//        fnatic.addTeamMember(new Player("Nico", "Mid"), fnaticRoster);
+//        fnatic.addTeamMember(new Player("KRIMZ", "Short"), fnaticRoster);
+//        fnatic.addTeamMember(new Player("mezii", "Short"), fnaticRoster);
+//        fnatic.addTeamMember(new Player("roej", "Long"), fnaticRoster);
+//        fnatic.addTeamMember(new Player("FASHR", "Long"), fnaticRoster);
+//        fnatic.printTeamMembers(fnaticRoster);
+//}
--- src/dev/pz/Team.java
+++ src/dev/pz/Team.java
@@ -1,10 +1,13 @@
 package dev.pz;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 enum GameOutcome {WON_GAME, TIED_GAME, LOST_GAME, FORFEIT, DELAYED}
+enum GameRole {MIDDLE, TOP, BOTTOM, SHORT, LONG, BANANA, A_SITE, B_SITE, JUNGLE}
 
-record Player(String playerName, String position){}
+record Player(String playerName, GameRole position){}
 
 public class Team<T>{
 
@@ -27,6 +30,8 @@ public class Team<T>{
     private static int tiedGames;
     private static int lostGames;
 
+    List<Player> teamRoster = new ArrayList<>();
+
     public Team(String teamName) {
         this.teamName = teamName;
     }
@@ -41,12 +46,34 @@ public class Team<T>{
         teamList.add(player);
     }
 
-    public void printTeamMembers(List<Player> teamList)
+    public void printTeamMembers()
     {
-        for(var itr : teamList) System.out.println(itr);
+        for(var itr : teamRoster) System.out.println(itr);
+    }
+
+
+    public Team<Game> initializeGameTeam(Player... players)
+    {
+
+        if (players.length !=5) throw new IllegalArgumentException("A game team must have exactly 5 players!");
+        Team<Game> team = new Team<Game>(teamName);
+        teamRoster = Arrays.asList(players);
+
+        return team;
     }
 
 
 
 
 }
+//    public Team<Game> initializeGameTeam(String teamName, Player player1, GameRole player1Role, Player player2, GameRole player2Role,
+//                                         Player player3, GameRole player3Role, Player player4, GameRole player4Role,
+//                                         Player player5, GameRole player5Role)
+//    {
+//        Team<Game> team = new Team<Game>(teamName);
+//        List <Player> teamRoster = new ArrayList<>(Arrays.asList(new Player(player1, player1Role),
+//                new Player(player2, player2Role), ));
+//
+//
+//        return team;
+//    }
